31f39aafc8ec8314786f3933453139c82a5b06d5
pham ngoc dat <phamngocdat12122000@gmail.com>
2023-03-15 00:40:15 +0700

    15/3/2023

---
+++ SumRootToLeafNumber.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+using namespace std;
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int sum = 0;
+int helper(TreeNode* root, string result)
+{
+    result.push_back(root->val+48);
+    if(root->left == NULL && root->right == NULL)
+        sum = sum + stoi(result);
+    else
+    {
+        if (root->left != NULL)
+            helper(root->left,result);
+        if(root->right != NULL)
+            helper(root->right,result);
+    }
+}
+int sumNumbers(TreeNode* root) {
+    helper(root,"");
+    cout<<sum;
+    return sum;
+}
+int main()
+{
+    TreeNode node1;
+    TreeNode node2;
+    TreeNode node3;
+    TreeNode node4;
+    node1.left = &node2;
+    node1.right = &node3;
+    node2.left = &node4;
+    node4.val = 4;
+    node1.val  = 1;
+    node2.val = 2;
+    node3.val = 3;
+    sumNumbers(&node1);
+
+}
---
+++ SymmetricTree.cpp
@@ -0,0 +1,130 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// https://leetcode.com/problems/symmetric-tree/
+class Solution {
+public:
+
+    vector<int> value_list;
+    map<int,int> node_number_level;
+    int get_height(TreeNode* root)
+    {
+        if(root != NULL)
+            return 1 + max(get_height(root->left),get_height(root->right));
+        else
+            return 0;
+    }
+    void helper(TreeNode* root,int level,int reference)
+    {
+        if(root != NULL)
+        {
+            if(level == 1)
+            {
+                value_list.push_back(root->val);
+                if(node_number_level.find(reference) != node_number_level.end())
+                {
+                    node_number_level[reference]++;
+                }
+                else
+                {
+                    node_number_level[reference] = 1;
+                }
+                return;
+            }
+            else
+            {
+                helper(root->left,level - 1,reference);
+                helper(root->right,level - 1,reference);
+            }
+        }
+        else
+        {
+            /* code */
+            if(level == 1)
+            {
+                if(node_number_level.find(reference) != node_number_level.end())
+                {
+                    node_number_level[reference]++;
+                }
+                else
+                {
+                    node_number_level[reference] = 1;
+                }
+                value_list.push_back(-101);
+            }
+        }
+    }
+    bool isSymmetric(TreeNode* root) {
+        int tree_height = get_height(root);
+        stack<int> stack_;
+        stack<int> index_;
+        vector<int> side_;
+        bool is_count =false;
+        int temp = 2;
+        if(root == NULL)
+        {
+            return false;
+        }
+        for(int i = 1;i <= tree_height; i++)
+        {
+            helper(root,i,i);
+        }
+        for(size_t i = 0 ; i < value_list.size(); i++)
+        {
+            side_.push_back(2);
+        }
+        for(size_t i = 1 ; i < value_list.size(); i++)
+        {
+            if(side_[i] == 2)
+            {
+                int temp1 = node_number_level[temp] / 2;
+                int temp2 = temp1;
+                int j = i;
+                while(temp1 != 0)
+                {
+                    side_[j] = 0;
+                    temp1--;
+                    j++;
+                }
+                while(temp2 != 0)
+                {
+                    side_[j] = 1;
+                    temp2--;
+                    j++;
+                }
+                temp++;
+            }
+        }
+        for(size_t i = 1 ; i < value_list.size(); i++)
+        {
+            if(!stack_.empty())
+            {
+                if(stack_.top() == value_list[i] && side_[i]!= side_[index_.top()])
+                {
+                    index_.pop();
+                    stack_.pop();
+                }
+                else
+                {
+                    stack_.push(value_list[i]);
+                    index_.push(i);
+                    /* code */
+                }
+            }
+            else
+            {
+                index_.push(i);
+                stack_.push(value_list[i]);
+            }
+        }
+        return stack_.empty();
+}
+};
